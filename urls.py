7c5770f5da7e6e4921205665d1447c662dd484b6
venki <69787622+venkai1234@users.noreply.github.com>
2024-02-02 15:46:22 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,26 +1,28 @@
-
 from django.contrib import admin
-from django.urls import path , include
-from django.urls import re_path as url
-from django.contrib.auth import views as auth_views
+from django.urls import path
 from django.conf.urls.static import static
+from . import views
+from django.conf.urls import url
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'),
-    path('', include('campus.urls')),
-    path('student/student_register/', include('campus.urls')),
-    path('student/student_login/', include('campus.urls')),
-    path('student/student_login/usd/', include('campus.urls')),
-    path('logout/',include('campus.urls')),
-    path('student/student_login/dispstu/', include('campus.urls')),
-    path('company/company_register/', include('campus.urls')),
-    path('company/company_login/', include('campus.urls')),
-    path('company/company_login/ccd/',include('campus.urls')),
-    path('company/company_login/jp/', include('campus.urls')),
-    path('company/company_login/jd/', include('campus.urls')),
-    path('company/company_login/viewpos/', include('campus.urls')),
-    path('student/student_login/applyjob/',include('campus.urls')),
-    path('student/student_login/applyjob/<str:opt>/',include('campus.urls')),
-    path('company/company_login/selectstu/', include('campus.urls')),
-    path('company/company_login/selectstu/<str:opt>/',include('campus.urls')),
+    path('student/student_register/' , views.student_register , name='student_register'),
+    path('student/student_login/', views.student_login,name='student_login'),
+    path('student/student_login/usd/',views.usd,name='usd'),
+    path('logout/', views.pagelogout, name='logout'),
+    path('student/student_login/dispstu/',views.dispstu,name='dispstu'),
+    path('company/company_register/', views.company_register, name='company_register'),
+    path('company/company_login/', views.company_login, name='company_login'),
+    path('company/company_login/ccd/', views.ccd, name='ccd'),
+    path('company/company_login/jp/', views.jobpos, name='jp'),
+    path('company/company_login/jd/', views.jd, name='jd'),
+    path('company/company_login/deletevacan/', views.deletevacan, name='deletevacn'),
+    path('company/company_login/viewpos/', views.viewpos, name='viewpos'),
+    path('student/student_login/applyjob/', views.applyjob, name='apply'),
+    path('student/student_login/applyjob/<str:opt>/', views.apply, name='apply1'),
+    path('', views.home, name='home'),
+    path('company/company_login/selectstu/', views.selectstu, name='sstu'),
+    path('company/company_login/selectstu/<str:opt>/',views.stumail,name="sm")
+    
 
 ]
